edfe7a5c4ab54ee275f8f92616e45b18ef084c29
Ashish0431 <aditi0421p@gmail.com>
2026-06-22 18:26:18 +0530

    Create faqs.js

---
+++ faqs.js
@@ -0,0 +1,26 @@
+const faqs = [
+{
+question:"what is codealpha",
+answer:"CodeAlpha is a software development company that offers internships and training programs."
+},
+{
+question:"how many tasks are required",
+answer:"You need to complete at least two tasks to be eligible for internship completion."
+},
+{
+question:"will i get a certificate",
+answer:"Yes, eligible interns receive a certificate after successful completion."
+},
+{
+question:"how can i submit my project",
+answer:"Projects should be submitted through the official submission form provided by CodeAlpha."
+},
+{
+question:"where should i upload my source code",
+answer:"Source code should be uploaded to GitHub in the required repository format."
+},
+{
+question:"what technologies can i use",
+answer:"You can use HTML, CSS, JavaScript, Python, Java, PHP, or any suitable technology."
+}
+];
